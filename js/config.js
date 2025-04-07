fbbee5eaa1fae9f138388b6c9c904264d7a45ce8
pasibun <matthijs_hovestad@hotmail.com>
2025-04-07 09:53:52 +0200

    wijigignen van andere repo meegenomen

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ let respecConfig = {
   useLogo: true,
   useLabel: true,
   // title is verplicht! Neem hier de titel van het document op ----------------------
-  title: "Conceptueel Informatiemodel Omgevingswet",
+  title: "[TODO: Neem titel op in config.js]",
   //-- specStatus is verplicht! (activeer 1 van de volgende) --------------------------
   specStatus: "wv",                   // Werkversie
   //specStatus: "cv",               // Consultatieversie
@@ -10,15 +10,29 @@ let respecConfig = {
   //specStatus: "def",              // Vastgestelde versie
   //specStatus: "basis",            // Basis Document
   //-- specType is verplicht bij alle andere dan BASIS ---------------------------------
-  specType: "IM",                 // Informatie Model
-  //-- GW20240906: url naar base.css toegevoegd
-  nl_organisationStylesURL: "https://tools.geostandaarden.nl/respec/style/",
+  //specType: "NO",                 // Norm
+  //specType: "ST",                 // Standaard
+  //specType: "IM",                 // Informatie Model
+  //specType: "PR",                 // Praktijkrichtlijn
+  specType: "HR",                     // HandReiking
+  //specType: "WA",                 // Werkafspraak
+  //specType: "BD",                 // Beheer Documentatie
+  //specType: "AL",                 // Algemeen document
+  //specType: "BP",                 // Best Practice
   //-- pubDomain is verplicht! (komt in de URL) -------------------------------------
-  pubDomain: "dso",
+  //-- zie: https://geonovum.github.io/handleiding-tooling/ReSpec/#pubdomain
+  //-- TODO: vul pubDomain in
+  pubDomain: "wp",
   //-- license: voor de geldende gebruiksvoorwaarden. Default is cc-by.
+  //license: "cc-by-nd",            // bronvermelding, geen afgeleide werken (default)
+  //license: "cc0",                 // Public Domain Dedication
   license: "cc-by",                 // Attribution, met bronvermelding
-  //-- shortName is verplicht! (komt in de URL: kies logische afkorting)--------------
-  shortName: "dso-cim-ow",
+  //-- TODO shortName is verplicht! (komt in de URL: kies logische afkorting)--------------
+  shortName: "NL-ReSpec-GN-template",
+  
+  //edDraftURI = De URI van de draft version. Deze wordt automatisch afgeleid van de github URI; maar kan hier overschreven worden. 
+	//edDraftURI: ["https://geonovum.github.io", "/", "shortName"],
+
   //-- publishDate is verplicht -------------------------------------------------------
   //-- NB: in de werkversie uitzetten, want dan pakt Respec de pushdate ---------------
   //publishDate: "2023-03-28",
@@ -28,12 +42,12 @@ let respecConfig = {
   //-- Voor dit blok geldt: alleen als er eerdere versies zijn en altijd beiden aan/uit! 
   //previousPublishDate: "2014-05-01",
   //previousMaturity: "CV",
-  //-- de namen van de Editor(s) / Redacteur(en)---------------------------------------
+  //-- TODO: de namen van de Editor(s) / Redacteur(en)---------------------------------------
   //-- vul in: per Editor: name:, company:, companyURL: -------------------------------
   editors:
     [
       {
-        name: "TPOD team",
+        name: "voornaam achternaam",
         company: "Geonovum",
         companyURL: "https://www.geonovum.nl",
       }
@@ -43,63 +57,35 @@ let respecConfig = {
   authors:
     [
       {
-        name: "TPOD team",
+        name: "voornaam achternaam",
         company: "Geonovum",
         companyURL: "https://www.geonovum.nl",
       }
     ],
+  // TODO: Vul de github URL in.
   //neem hier de URL van de github repository op waar het respec document in staat
-  github: "https://github.com/Geonovum/dso-cim-ow",
+  github: "https://github.com/Geonovum/NL-ReSpec-GN-template",
   // Create PDF and link to file in header (optional):
+  // TODO: Change the filename as preferred.
   alternateFormats: [
       {
           label: "pdf",
-          uri: "dso-cim-ow.pdf",
+          uri: "template.pdf",
       },
   ],
-    //-- localBiblio: lokale bibliografie, voor verwijzigingen
-  //-- NB: kijk eerst naar de beschikbare www.specref.org voor verwijziging 
+  // Lokale lijst voor bibliografie
+  // - Kijk eerst naar de beschikbare www.specref.org .
+  // - Kijk daarna in de organisatieconfig.
+  // - Voeg dan pas hieronder toe.
   localBiblio: 
   {
-    Basisgeometrie: {
-      id: "Basisgeometrie",
-      title: "Basisgeometrie",
-      href: "https://docs.geostandaarden.nl/nen3610/def-st-basisgeometrie-20200930/",
+    MIM12: {
+      id: "MIM12",
+      title: "MIM - Metamodel Informatie Modellering (Versie 1.2)",
+      href: "https://docs.geostandaarden.nl/mim/def-st-mim-20240613/",
       status: "Definitief",
       publisher: "Geonovum",
-      date: "30 september 2020"
-    },
-    MIM: {
-      id: "MIM",
-      title: "MIM - Metamodel Informatie Modellering",
-      href: "https://docs.geostandaarden.nl/mim/def-st-mim-20220217/",
-      status: "Definitief",
-      publisher: "Geonovum",
-      date: "2022-02-17"
-    },
-    CIMOW: {
-      id: "CIMOW",
-      title: "Conceptueel Informatiemodel Omgevingswet (CIMOW)",
-      href: "https://geonovum.github.io/TPOD/CIMOW/CIMOW_v2.1.0.pdf",
-      status: "Definitief",
-      publisher: "Geonovum",
-      date: "2022-01-07"
-    },
-    IMOW: {
-      id: "IMOW",
-      title: "Informatiemodel Omgevingswet (IMOW)",
-      href: "https://geonovum.github.io/TPOD/CIMOW/IMOW_v2.0.2.pdf",
-      status: "Definitief",
-      publisher: "Geonovum",
-      date: "2023-01-09"
-    },
-    STOP20: {
-      id: "STOP20",
-      title: "Standaard Officiële Publicaties",
-      href: "https://koop.gitlab.io/STOP/standaard/2.0.0-rc/index.html",
-      status: "Release Candidate",
-      publisher: "KOOP",
-      date: "2023-02-17"
-    },
-  },
+      date: "2024-06-13"
+    }
+  }
 };
