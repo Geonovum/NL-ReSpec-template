790b13d03777e87311d2695106507d9c341b7880
Wilko Quak <w.quak@geonovum.nl>
2025-10-08 11:45:40 +0200

    config.js aangepast aan nieuwe Logius ReSpec fixes #6

--- js/config.js
+++ js/config.js
@@ -1,49 +1,57 @@
 let respecConfig = {
   useLogo: true,
   useLabel: true,
+
   // TODO: title is verplicht! Neem hieronder de titel van het document op
   title: "Geonovum template voor ReSpec",
-  //-- specStatus is verplicht! (activeer 1 van de volgende) --------------------------
-  specStatus: "wv",                   // Werkversie
+  //-- specStatus is verplicht! (activeer 1 van de volgende)
+  specStatus: "wv",                 // Werkversie
   //specStatus: "cv",               // Consultatieversie
   //specStatus: "vv",               // Versie ter vaststelling
   //specStatus: "def",              // Vastgestelde versie
   //specStatus: "basis",            // Basis Document
-  //-- specType is verplicht bij alle andere dan BASIS ---------------------------------
+
+  //-- specType is verplicht bij alle andere dan BASIS
   //specType: "NO",                 // Norm
   //specType: "ST",                 // Standaard
   //specType: "IM",                 // Informatie Model
   //specType: "PR",                 // Praktijkrichtlijn
-  specType: "HR",                     // HandReiking
+  specType: "HR",                   // HandReiking
   //specType: "WA",                 // Werkafspraak
   //specType: "BD",                 // Beheer Documentatie
   //specType: "AL",                 // Algemeen document
   //specType: "BP",                 // Best Practice
-  //-- pubDomain is verplicht! (komt in de URL) -------------------------------------
+
+  //-- pubDomain is verplicht! (komt in de URL)
   //-- zie: https://geonovum.github.io/handleiding-tooling/ReSpec/#pubdomain
   //-- TODO: vul pubDomain in
   pubDomain: "3dbv",
+
   //-- license: voor de geldende gebruiksvoorwaarden. Default is cc-by.
   //license: "cc-by-nd",            // bronvermelding, geen afgeleide werken (default)
   //license: "cc0",                 // Public Domain Dedication
   license: "cc-by",                 // Attribution, met bronvermelding
-  //-- TODO shortName is verplicht! (komt in de URL: kies logische afkorting)--------------
+
+  //-- TODO shortName is verplicht! (komt in de URL: kies logische afkorting)
   shortName: "NL-ReSpec-template",
   
   //edDraftURI = De URI van de draft version. Deze wordt automatisch afgeleid van de github URI; maar kan hier overschreven worden. 
 	//edDraftURI: ["https://geonovum.github.io", "/", "shortName"],
 
-  //-- publishDate is verplicht -------------------------------------------------------
-  //-- NB: in de werkversie uitzetten, want dan pakt Respec de pushdate ---------------
-  //publishDate: "2025-03-28",
-  //eventueel is het mogelijk een versienummer mee te geven, maar bij Geonovum werken we gewoonlijk alleen met datum als onderdeel van de permanente URI.
-  //publishVersion: "0.0.2",
+  //-- publishDate is verplicht. Als je werkversie gekozen hebt  dan pakt Respec
+  //-- de pushdate maar de publishDate is nog steeds verplicht.
+  publishDate: "2025-03-28",
+  
+  //-- publishVersion is verplicht. Hij mag wel leeg zijn [], maar niet de lege string zijn "".
+  publishVersion: [],
+ 
+  //-- Voor dit blok geldt: alleen als er eerdere versies zijn en altijd allemaal aan/uit! 
   //previousVersion: "0.0.1",
-  //-- Voor dit blok geldt: alleen als er eerdere versies zijn en altijd beiden aan/uit! 
   //previousPublishDate: "2014-05-01",
   //previousMaturity: "CV",
-  //-- TODO: de namen van de Editor(s) / Redacteur(en)---------------------------------------
-  //-- vul in: per Editor: name:, company:, companyURL: -------------------------------
+
+  //-- TODO: de namen van de Editor(s) / Redacteur(en)
+  //-- vul in: per Editor: name:, company:, companyURL:
   editors:
     [
       {
@@ -52,8 +60,9 @@ let respecConfig = {
         companyURL: "https://www.geonovum.nl",
       }
     ],
-  //-- de namen van de auteur(s) ------------------------------------------------------
-  //-- vul in: per auteur: name:, company:, companyURL: -------------------------------
+
+  //-- de namen van de auteur(s) 
+  //-- vul in: per auteur: name:, company:, companyURL: 
   authors:
     [
       {
@@ -62,9 +71,11 @@ let respecConfig = {
         companyURL: "https://www.geonovum.nl",
       }
     ],
+
   // TODO: Vul de github URL in.
   //neem hier de URL van de github repository op waar het respec document in staat
   github: "https://github.com/Geonovum/NL-ReSpec-template",
+
   // Create PDF and link to file in header (optional):
   // TODO: Change the filename as preferred.
   alternateFormats: [
@@ -73,6 +84,7 @@ let respecConfig = {
           uri: "static/template.pdf",
       },
   ],
+
   // Lokale lijst voor bibliografie
   // - Kijk eerst naar de beschikbare www.specref.org .
   // - Kijk daarna in de organisatieconfig.
